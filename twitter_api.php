8ead16e7c24d78c82c69893675524e0b8d7fa17d
hato6502 <erisuke12345@yahoo.co.jp>
2018-02-05 12:52:23 +0900

    [del] 使わなくなった古いファイル

--- twitter_api.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('CONSUMER_KEY', 'u713k0eElQz84YSWbFsXGMk5G');
-	define('CONSUMER_SECRET', 'nhcbZ91YGMrJ7aYyKoznAeJx87YOoShLIYY7KEc131hMxwxwK3');
-	define('OAUTH_CALLBACK', DOMAIN.ROOT_URL.'callback.php');
-?>
-
