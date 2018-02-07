5665e183787619cd72bea762a3942cd24225d6a1
hato6502 <erisuke12345@yahoo.co.jp>
2018-02-07 14:26:51 +0900

    [fix] アルバムの画像表示サイズ

--- user/album/action.php
+++ user/album/action.php
@@ -18,7 +18,7 @@
 		<?php include(ROOT_PATH.'header.php'); ?>
 		<h2 class="topbar">アルバムを使う</h2>
 		<div class="main" style="text-align: center; ">
-			<img src="<?php echo htmlspecialchars($_GET['img']); ?>" style="max-width: 90%; -webkit-filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.2)); "><br>
+			<img src="<?php echo htmlspecialchars($_GET['img']); ?>" style="max-height: 70vh; max-width: 90%; -webkit-filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.2)); "><br>
 			<a href="<?php echo ROOT_URL; ?>tweet/diary/?<?php echo http_build_query(['album' => $_GET['id']]); ?>" class="a-disabled"><div class="card" style="display: inline-block; "><div class="card-article">つぶやきを投稿する</div></div></a>
 			<a href="<?php echo ROOT_URL; ?>tweet/draw/?<?php echo http_build_query(['album' => $_GET['id']]); ?>" class="a-disabled"><div class="card" style="display: inline-block; "><div class="card-article">お絵かきを投稿する</div></div></a>
 		</div>
