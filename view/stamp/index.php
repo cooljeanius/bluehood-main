2fd504df83641861ec59a7241faa7a2965dd75b9
hato <erisuke12345@yahoo.co.jp>
2018-03-19 23:46:18 +0900

    [fix] スタンプ3Dプリント微調整

--- view/stamp/index.php
+++ view/stamp/index.php
@@ -8,7 +8,9 @@
 		'make' => ['ja' => "スタンプを作る", 'en' => "Create a stamp", ],
 		'selected' => ['ja' => "手持ちのスタンプ", 'en' => "Selected stamps", ],
 		'add' => ['ja' => "追加", 'en' => "Add", ],
+		'dl' => ['ja' => "3D ダウンロード", 'en' => "3D Download", ],
 		'remove' => ['ja' => "削除", 'en' => "Remove", ],
+		'help_3d' => ['ja' => "3D ダウンロードとは? ", 'en' => "What's 3D Download? ", ],
 		//'' => ['ja' => "", 'en' => "", ],
 	];
 ?>
@@ -20,11 +22,9 @@
 	<body>
 		<div class="topbar"><?php l($s['title']); ?></div>
 		<div class="main">
-			<div class="header" style="position: relative; ">
+			<div class="header">
 				<a href="<?php echo ROOT_URL; ?>tweet/stamp/" class="linkbutton" style="position: absolute; top: 0; right: 0; "><?php l($s['make']); ?></a>
 				作ったスタンプをお絵かき投稿で使用できます。
-			</div>
-			<div class="header">
 				<div style="text-align: center; "><?php l($s['selected']); ?><br>
 					<?php
 						mysql_start();
@@ -41,6 +41,9 @@
 					?>
 				</div>
 			</div>
+			<div class="paddingleft paddingright">
+				<span style="font-size: small; "><?php l($s['help_3d']); ?></span><?php helpbutton('スタンプの 3D モデルを stl ファイルでダウンロードできます。\n3D プリントすれば、現実世界で使えるかも? '); ?>
+			</div>
 			<div style="text-align: center; "><?php
 				$request = ['id' => 'custom-'.COLLECTON_STAMP, 'count' => '200'];
 				if (isset($_GET['i'])) $request['max_position'] = $_GET['i'];
@@ -51,7 +54,8 @@
 					?>
 					<div style="display: inline-block; width: 240px; ">
 						<div id="stamp-<?php echo $i; ?>"></div>
-						<a href="add.php?<?php echo http_build_query(['image_url' => $status->entities->media[0]->media_url_https]); ?>"><button><?php l($s['add']); ?></button></a><br>
+						<a href="add.php?<?php echo http_build_query(['image_url' => $status->entities->media[0]->media_url_https]); ?>"><button><?php l($s['add']); ?></button></a>
+						<a href="stlstamp/?<?php echo http_build_query(['stamp' => basename($status->entities->media[0]->media_url_https)]); ?>"><button><?php l($s['dl']); ?></button></a><br>
 						<br>
 					</div>
 					<script>$(function(){
